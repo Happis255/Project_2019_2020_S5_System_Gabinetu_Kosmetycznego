f6af86c3d45004c0ef117fb860314beff9aeec77
Happis255 <hubertwasik27@hotmail.com>
2020-01-06 02:59:30 +0100

    4.1.3 - workerzy - w trakcie tworzenia Nie działa mi guzik do usuwania uprawnienia

---
+++ src/myPage/servlets/ControllerRemoveServWork.java
@@ -0,0 +1,50 @@
+package myPage.servlets;
+
+import myPage.basicObjects.Usluga;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name =  "kontroler służący do usuwania zaznaczonych z listy nieobecności", urlPatterns = { "/ControllerRemoveServWork" })
+public class ControllerRemoveServWork extends HttpServlet {
+
+    private HttpSession session;
+    private String resultMessage = "";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        session = request.getSession();
+
+        Usluga uslugi = new Usluga();
+        String[] spis_id = request.getParameterValues("do_usuniecia");
+        int a = 0;
+        String[] dane;
+        try {
+            for (int i = 0; i < spis_id.length; i++){
+
+                /* Rozdzielamy na ID Uslugi i ID Pracownika */
+                dane = spis_id[i].split(" ");
+                uslugi.removeAllowence(Integer.parseInt(dane[0]), Integer.parseInt(dane[1]));
+            }
+
+            resultMessage = "<h2 class=\"text-center\" style=\"height:53px;\">Uprawnienie do wykonywania usługi zostało odebrane.</h2><h5 class=\"text-center\" style=\"height:99px;margin-right:50px;margin-left:50px;\"><br>Zmiany powinny być już widoczne.<br></h5> <div class=\"form-group\"><a href=\"ControllerAccount?page=pracownicy\"><button class=\"btn btn-primary\" type=\"submit\" style=\"margin:0;width:265px;margin-left:267px;\">Powrót do pracowników</button></a></div>";
+            request.setAttribute("message", resultMessage);
+            getServletContext().getRequestDispatcher("/index_result.jsp").forward(request, response);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resultMessage = "<h2 class=\"text-center\" style=\"height:53px;\">Wystąpił błąd podczas modyfikacji uprawnienia.</h2><h5 class=\"text-center\" style=\"height:99px;margin-right:50px;margin-left:50px;\"><brProsimy o skontaktowanie się z administratorem systemu<br>bądź zgłoszenie błędu.<br></h5> <div class=\"form-group\"><a href=\"ControllerAccount?page=pracownicy\"><button class=\"btn btn-primary\" type=\"submit\" style=\"margin:0;width:265px;margin-left:267px;\">Powrót do pracowników</button></a></div>";
+            request.setAttribute("message", resultMessage);
+            getServletContext().getRequestDispatcher("/index_result.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
